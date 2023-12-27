bdd4c5b73f0f160eca7980e68331b1b5f8b95bb6
akshat <akshatbhatya@gmail.com>
2023-12-27 17:24:21 +0530

    creating header

--- portfolio/src/Components/Header/Header.jsx
+++ portfolio/src/Components/Header/Header.jsx
@@ -3,7 +3,52 @@ import "./Header.css"
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <>
+            <div className="header-parent">
+
+                <div className="header-section-one">
+                    <div className="header-section-one-logo">
+
+                    </div>
+                    <div className="header-section-one-headding">
+                        <h1>Be Creative,
+                            Be Impressive</h1>
+                        <br />
+                        <p>Creative Designer and Social Media Officer</p>
+                    </div>
+                    <div className="header-section-one-paragraph">
+
+                        <p>I have to be able to communicate ideas to a wide range of stakeholders in both my own voice and in Sprout's. Communicating on social.</p>
+
+                        <div className="header-section-one-buttons">
+                            <button className='btn-1'>Portfolio</button>
+                            <a href='' className='btn-2'>Download Curriculum Vitae</a>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="header-section-two">
+
+
+                    <div className="header-section-two-navigation">
+
+                        <nav>
+                            <ul className='header-navigation'>
+                                <li><a href="">Home</a></li>
+                                <li><a href="">About</a></li>
+                                <li><a href="">Project</a></li>
+                                <li><a href="">Contact</a></li>
+                                <li></li>
+                            </ul>
+                        </nav>
+                    </div>
+
+                    <img src="" alt="" />
+
+                </div>
+
+            </div>
+        </>
     )
 }
 
