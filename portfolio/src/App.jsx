72a7656d636a34dbd0eddedd45a5ae0f3ed3aeb7
akshat <akshatbhatya@gmail.com>
2023-12-31 16:15:15 +0530

    title settings

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,24 @@
+import { useEffect } from 'react'
 import './App.css'
 import { Header } from "./Components/Index"
 import { Outlet } from "react-router-dom"
 
 function App() {
+ 
 
+  useEffect(() => {
+  
+    const handleVisibilityChange = () => {
+      document.title = document.hidden ? '😢 Akshat | Web Developer ' : '😊 Akshat | Web Developer ';
+    };
+
+     document.addEventListener('visibilitychange', handleVisibilityChange);
+
+  }, []);
 
   return (
+
+    
     <>
       <Header />
     </>
